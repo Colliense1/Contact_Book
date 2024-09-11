42a07270c18f8af1055db1dfd612e77f51abd7e6
Colliense Podder Prodip <colliensepodder25@gmail.com>
2024-09-11 23:33:09 +0600

    commit

--- create_contact.py
+++ create_contact.py
@@ -3,6 +3,10 @@ def create_contact_func(contacts_book):
     phone = input("Enter Phone: ")
     email = input("Enter Email: ")
 
+    if not name or phone or email:
+        print("Invalid input. Please try again.")
+        return
+
     contact = {
         "name": name,
         "phone": phone,
